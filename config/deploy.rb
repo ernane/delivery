f870e81aea9eb42e876e151dd77f4510da357457
Ernane Sena <ernane.sena@gmail.com>
2012-06-29 16:54:47 -0300

    Add files Capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,37 @@
+
+server "173.246.40.14", :web, :app, primary: true
+
+set :application, "delivery"
+set :user, "deployer"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+set :scm, "git"
+set :repository, "git@github.com:ernane/#{application}.git"
+set :branch, "master"
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
+namespace :deploy do
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/apache.conf /etc/apache2/sites-available/#{application}"
+    run "mkdir -p #{shared_path}/config"
+    run "ln -s /home/#{user}/Zend #{current_path}/library/Zend"
+  end
+  after "deploy:setup", "deploy:setup_config"
+ 
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
+end
